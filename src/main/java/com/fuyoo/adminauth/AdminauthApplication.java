a4b2eea4da34d4fea83642a98f7f2ebcd346e0ff
wangsf <cs-afu@163.com>
2022-03-31 09:27:19 +0800

    初始工程提交

---
+++ src/main/java/com/fuyoo/adminauth/AdminauthApplication.java
@@ -0,0 +1,13 @@
+package com.fuyoo.adminauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminauthApplication.class, args);
+    }
+
+}
